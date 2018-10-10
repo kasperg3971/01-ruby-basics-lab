63893434fdd07cf231e6669134b484d0f61d9251
Kasper Gacek <kasperg3971@hstat.org>
2018-10-10 03:15:34 +0000

    fixed ruby_basics.rb

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,35 @@
-def division
+def division(num1, num2)
+    
+    puts num1/num2
+end
+
+def argue(argument)
+    puts argument
     
 end
 
-def argue
-  
-end
 
-def greeting
+def greeting(greeting, name)
+    puts greeting + " " + name
 
 end
 
 def return_a_value
-    
+    puts "Nice"
 end
 
-def pizza_party
-    
+
+def pizza_party(topping = "cheese")
+    puts topping
 end
 
-def whisper
+def whisper(word)
+    puts word.downcase
     
 end
+division(100,20)
+argue("I disagree")
+greeting("hello", "kasper")
+return_a_value
+pizza_party
+whisper("I AM YOUR WORST NIGHTMARE")
